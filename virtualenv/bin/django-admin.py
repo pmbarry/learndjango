11be700954bcf7f23a4641129de31e7200f178ef
Paul Barry <paulbarry@Pauls-MacBook-Pro.local>
2019-06-02 17:44:44 +0100

    First commit

---
+++ virtualenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/paulbarry/Documents/DjangoProjects/python-tdd-book/virtualenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
